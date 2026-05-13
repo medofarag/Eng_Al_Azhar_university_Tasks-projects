8692de2ec391646cf3397028a2d0e50d7627b915
Mahmoud Farag <medofarag@proton.me>
2026-05-13 14:21:36 +0300

    add bubblesort

--- simplesort/bubblesort.java
+++ simplesort/bubblesort.java
@@ -1,28 +1,28 @@
-public class bubblesort{
-    public static void bubblesort(int[] arr){
-	int size = arr.length;
-	for(int i = 0; i < size-1; i++){
-	    for(int j = 0; j < size-i-1 ; j++){
-		if(arr[j] > arr[j+1]){
-		    swap(arr, j, j+1);
-		}
-	    }
-	}
+public class bubblesort {
+  public static void bubbleSort(int[] arr) {
+    int size = arr.length;
+    for (int i = 0; i < size - 1; i++) {
+      for (int j = 0; j < size - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          swap(arr, j, j + 1);
+        }
+      }
     }
-    
-    public static void swap(int[] arr, int i , int j){
-	int temp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = temp;
-    }
-    
-    public static void main(String[] args){
-	int[] arr1 = {33,55,35,357,47,245,888,999,23,466,7};
+  }
 
-	bubblesort(arr1);
+  public static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
 
-	for(int k = 0; k < arr1.length; k++){
-	    System.out.print(arr1[k] + " ");
-	}
+  public static void main(String[] args) {
+    int[] arr1 = { 33, 55, 35, 357, 47, 245, 888, 999, 23, 466, 7 };
+
+    bubbleSort(arr1);
+
+    for (int k = 0; k < arr1.length; k++) {
+      System.out.print(arr1[k] + " ");
     }
+  }
 }
